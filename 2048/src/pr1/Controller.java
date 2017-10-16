2a0c380e65bbd9da63ef353f7ca935d2191d1bbe
diegoambite <diegoambite@gmail.com>
2017-10-16 11:58:25 +0200

    first steps in reading the input data

--- 2048/src/pr1/Controller.java
+++ 2048/src/pr1/Controller.java
@@ -1,5 +1,6 @@
 package pr1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -15,8 +16,33 @@ public class Controller {
 	private Game game;
 	private Scanner in;
 	
+	//variable to store the conditions of the game to be developed
+	public int sizeBoard;
+	public int numDigit;
+	
+	
+	public Controller(Game  currentGame) {
+		currentGame = game;
+		in = new Scanner(System.in);
+	}
+	
+	//SHOULD BE MODIFIED IN THE FUTURE IN ORDER TO ACCEPT THE 3RD PARAMETER, THE ONE CONCERNING TO THE SEED
+	public void firstStep(){
+		sizeBoard = in.nextInt();
+		numDigit = in.nextInt();
+			
+	}
+	
+	
+	
+	
+	
 	
 	public void run(){
+		//read the first input
+		firstStep();
+		//creates a new game with the values preiously introduced
+		game = new Game(sizeBoard, numDigit, null);
 		
 	}
 }
