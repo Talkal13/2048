aac82b7e6f3a1bf605f38eeda688ad5192921e3c
Talkal13 <pavazq01@ucm.es>
2017-11-22 23:57:14 +0100

    added missing indexes

---
+++ 2048/src/p2/control/commands/NoParamsCommand.java
@@ -0,0 +1,24 @@
+package p2.control.commands;
+
+import p2.control.Controller;
+import p2.logic.multigames.Game;
+
+public class NoParamsCommand extends Command {
+
+	public NoParamsCommand(String commandInfo, String helpInfo) {
+		super(commandInfo, helpInfo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(Game game, Controller controller) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Command parse(String[] commandWords, Controller controller) {
+		if (commandWords[0].equals(this.commandName)) return this;
+		return null;
+	}
+
+}
